1803ceb863817f0945600a116a2367188f8c25ea
SeongUkJang <seonguk94@gmail.com>
2025-03-19 15:52:10 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-calculator', // 필수: GitHub repo 이름
+  base: '/react-calculator/', // 필수: GitHub repo 이름
   build: {
     outDir: 'docs', // ← build 결과를 docs 폴더로!
   },
